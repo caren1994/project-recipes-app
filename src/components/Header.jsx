a097b44caf2751b18520981fa1c4b56ac6ea0f0a
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2022-10-24 20:01:01 -0300

    requisito 7 fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,38 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
-import { ReactComponent as SearchIcon } from '../images/searchIcon.svg';
-import { ReactComponent as ProfileIcon } from '../images/profileIcon.svg';
+import SearchIcon from '../images/searchIcon.svg';
+import ProfileIcon from '../images/profileIcon.svg';
 import SearchBar from './SearchBar';
 
 function Header({ title }) {
   const history = useHistory();
   const [showInput, setShowInput] = useState(false);
+  const renderSearchIcon = () => history.location.pathname !== '/profile'
+    && history.location.pathname !== '/done-recipes'
+    && history.location.pathname !== '/favorite-recipes';
+
   return (
     <section>
       <h2 data-testid="page-title">{title}</h2>
-      <ProfileIcon
-        data-testid="profile-top-btn"
+      <button
+        type="button"
         onClick={ () => history.push('/profile') }
-      />
-      <SearchIcon
-        data-testid="search-top-btn"
-        onClick={ () => setShowInput(!showInput) }
-      />
+      >
+        <img src={ ProfileIcon } alt="Profile Icon" data-testid="profile-top-btn" />
+      </button>
+      { renderSearchIcon() && (
+        <button
+          type="button"
+          onClick={ () => setShowInput(!showInput) }
+        >
+          <img
+            data-testid="search-top-btn"
+            src={ SearchIcon }
+            alt="Search Icon"
+          />
+        </button>
+      )}
       {showInput && <SearchBar />}
     </section>
   );
