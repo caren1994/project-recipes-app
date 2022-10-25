b4b93983168b01f8df1030f8011186e1f8611606
Thales Santos <thalis.mraz@gmail.com>
2022-10-24 23:33:29 -0300

    test ate o 8

--- src/tests/login.test.js
+++ src/tests/login.test.js
@@ -4,21 +4,51 @@ import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
-describe('Lgin test', () => {
-  test('Login Test', () => {
+describe('Login screen tests', () => {
+  it('Checks if the email input appears on the screen and if it is possible to type in it', () => {
     renderWithRouter(<App />);
 
     const inputEmail = screen.getByTestId('email-input');
     expect(inputEmail).toBeInTheDocument();
+    userEvent.type(inputEmail, 'grupo7@grupo7.com');
+  });
+
+  it('Checks if the password input appears on the screen and if it is possible to type in it', () => {
+    renderWithRouter(<App />);
 
     const inputPassword = screen.getByTestId('password-input');
     expect(inputPassword).toBeInTheDocument();
+    userEvent.type(inputPassword, '1234567');
+  });
+
+  it('Checks if the button contains the text "Enter"', () => {
+    renderWithRouter(<App />);
+
+    const textButtonEnter = screen.getByText(/Enter/i);
+    expect(textButtonEnter).toBeInTheDocument();
+  });
+
+  it('Checks if the Enter button can be clicked', () => {
+    renderWithRouter(<App />);
 
     const buttonEnter = screen.getByTestId('login-submit-btn');
     expect(buttonEnter).toBeInTheDocument();
 
-    userEvent.type(inputEmail, 'grupo7@grupo7.com');
-    userEvent.type(inputPassword, '1234556');
     userEvent.click(buttonEnter);
+  });
+
+  it('If clicking on the button redirects to /meals', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const inputEmail = screen.getByTestId('email-input');
+    const inputPassword = screen.getByTestId('password-input');
+    const buttonEnter = screen.getByTestId('login-submit-btn');
+
+    userEvent.type(inputEmail, 'grupo7@grupo7.com');
+    userEvent.type(inputPassword, '1234567');
+    userEvent.click(buttonEnter);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/meals');
   });
 });
