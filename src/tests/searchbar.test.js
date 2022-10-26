b7d753c5838947ffc98375c1a647b5a1a2e013e2
Aline Grance <grance.aline@gmail.com>
2022-10-25 21:49:56 -0300

    add some more tests

--- src/tests/searchbar.test.js
+++ src/tests/searchbar.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { screen, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
 import renderWithRouter from './renderWithRouter';
-import Meals from '../components/Header';
-import Drinks from '../pages/Drinks';
-import { mealIngredientChicken } from './mealmock';
+import App from '../App';
+import { mealFirstLetterM, mealIngredientChicken, mealNameRisotto } from './mealmock';
 import AppProvider from '../context/AppProvider';
 
 describe('test search bar component', () => {
@@ -13,11 +13,16 @@ describe('test search bar component', () => {
       json: async () => mealIngredientChicken,
     }));
 
-    const { debug } = renderWithRouter(
+    const { history } = renderWithRouter(
       <AppProvider>
-        <Meals />
+        <App />
       </AppProvider>,
     );
+
+    act(() => {
+      history.push('/meals');
+    });
+
     const searchIcon = screen.getByRole('img', {
       name: /search icon/i,
     });
@@ -27,21 +32,81 @@ describe('test search bar component', () => {
       name: /procurar:/i,
     });
     const ingredient = screen.getByText(/ingredient/i);
-    // const name = screen.getByText(/name/i);
-    // const firstLetter = screen.getByText(/first letter/i);
     const searchBtn = screen.getByText('Search');
 
     userEvent.click(ingredient);
-    userEvent.type(searchInput, 'beef');
+    userEvent.type(searchInput, 'chicken');
     userEvent.click(searchBtn);
 
-    waitFor(async () => {
-      const headings = await screen.findAllByRole('heading', { level: 3 });
-      console.log(headings);
-      // headings.forEach((heading) => {
-      //   expect(heading).not.toBeInTheDocument();
+    await screen.findByText('Brown Stew Chicken');
+    expect(global.fetch).toHaveBeenCalledWith('https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken');
+    jest.clearAllMocks();
+  });
+
+  it('Search Meal By Name', async () => {
+    global.fetch = jest.fn(async () => ({
+      json: async () => mealNameRisotto,
+    }));
+
+    const { history } = renderWithRouter(
+      <AppProvider>
+        <App />
+      </AppProvider>,
+    );
+
+    act(() => {
+      history.push('/meals');
     });
 
-    debug();
+    const searchIcon = screen.getByRole('img', {
+      name: /search icon/i,
+    });
+    userEvent.click(searchIcon);
+
+    const searchInput = screen.getByRole('textbox', {
+      name: /procurar:/i,
+    });
+    const name = screen.getByText(/name/i);
+    const searchBtn = screen.getByText('Search');
+    userEvent.click(name);
+    userEvent.type(searchInput, 'risotto');
+
+    act(() => {
+      userEvent.click(searchBtn);
+    });
+    jest.clearAllMocks();
+  });
+
+  it('Search Meal By First Letter', async () => {
+    global.fetch = jest.fn(async () => ({
+      json: async () => mealFirstLetterM,
+    }));
+
+    const { history } = renderWithRouter(
+      <AppProvider>
+        <App />
+      </AppProvider>,
+    );
+
+    act(() => {
+      history.push('/meals');
+    });
+
+    const searchIcon = screen.getByRole('img', {
+      name: /search icon/i,
+    });
+    userEvent.click(searchIcon);
+
+    const searchInput = screen.getByRole('textbox', {
+      name: /procurar:/i,
+    });
+    const firstLetter = screen.getByText(/first letter/i);
+    const searchBtn = screen.getByText('Search');
+    userEvent.click(firstLetter);
+    userEvent.type(searchInput, 'm');
+    userEvent.click(searchBtn);
+
+    await screen.findByText('Mediterranean Pasta Salad');
+    jest.clearAllMocks();
   });
 });
